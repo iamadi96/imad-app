d210b31563b1d8ce1348e037e4bb23675744ef26
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-16 16:47:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element = document.getElementById("mainText");
 element.innerHTML = "New Value";
+
+//make the image move
+
+var img = document.getElementById("yala");
+img.onclick = function() {
+  img.style.marginLeft = "100px";  
+}; 
