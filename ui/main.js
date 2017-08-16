40154d4362550fed52c4fd32d7eda1775a96d1b4
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-16 16:52:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,13 @@ element.innerHTML = "New Value";
 //make the image move
 
 var img = document.getElementById("yala");
+
 var marginLeft = 0;
 function moveRight(){
-    marginLeft+=10;
+    marginLeft= marginLeft + 10;
     img.style.marginLeft= marginLeft + "px";
 }
 
 img.onclick = function() {
-  var interval = setInterval(moveRight,50)  
+  var interval = setInterval(moveRight, 50);  
 }; 
