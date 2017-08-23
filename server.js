b6cda749e967a97e6e3bab3b5312c7f04ee3324c
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-24 00:55:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,75 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-/*var articles ={
-    'article-one' :{
-        title:"Article-one | Adi",
-        heading:"Profile",
-        content:`Hi i'm Adi and this is my profile.`
-        },
-    'article-two' :{
-        title:"Article-two | Adi",
-        heading:"Hobbies",
-        content:`Playing cricket, developiong apps and following MotoGp.`
-        },
-    'article-three' :{
-        title:"Article-three | Adi",
-        heading:"Experience",
-        content:`Worked at a startup called SCRAPLABS!`
-        }    
-};   
-function createTemplate (data){    
-    var title = data.title;
-    var heading = data.heading;
-    var content = data.content;
-    var htmlTemplate = 
-    `<html>
-    <head>
-     <title>
-     ${title}
-     </title>   
-     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
-    
-    <body>
-        <div>
-            <a href="/">Home</a>
-            <hr/>
-        </div>
-            <h1 style="color:red">
-            ${heading}
-            </h1>
-        <div>
-            ${content}
-        </div>
-    </body>
-    
-    </html>
-    `;
-    return htmlTemplate;
+var articleOne = {
+title : "Aritcle One",    
+heading : "Aritcle One",    
+date : "Aug 5 2017",
+content : `
+        <p>
+            Hello! I'm Aditya Pandey and i'm a student at bharati Vidyapeeth's College Of Engineering.
+        </p>
+        <p>
+            I'm 20 years old and live in Delhi.
+        </p>`    
 };
-*/
+var template = function(data){
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+var htmlTemplate = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+    ${title}
+    </title>
+    <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="container">
+        <a href="/">
+        Home
+        </a>
+        <hr/>
+        <h1>
+        ${heading}
+        </h1>
+        <div>
+        ${date}
+        </div>
+        <div>
+        ${content}  
+        </div>
+    </div>
+   </body>
+</html>
+`;
+return htmlTemplate;
+};
+    
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-/*
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-}); */ 
-
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'aritcle-one.html'));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'aritcle-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'aritcle-three.html'));
+  res.send(template(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
