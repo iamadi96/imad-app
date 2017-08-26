c0524d14be88011079457fcb8e8be008b00a0f68
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-26 14:11:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,14 +79,16 @@ var template = function(data){
                         return htmlTemplate;
 };
 
-counter = 0;    
-app.get('/counter', function(req, res){
-   counter = counter + 1;
-   res.send(counter.toString());
-});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter = 0;    
+app.get('/counter', function(req, res){
+   counter = counter + 1;
+   res.send(counter.toString());
 });
 
 var names =[];
