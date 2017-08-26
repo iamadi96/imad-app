5865d7ca9172665bc236f8d2bde25050a8ec9242
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-26 13:44:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,20 @@ button.onclick = function(){
 request.open('GET', 'http://ap96adi.imad.hasura-app.io/counter', true);
 request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //make a request to the server to send names.
+    
+    //capture the list of names and render it as a list.
+    var names =['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i =0; i<list.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML= list;
+};
