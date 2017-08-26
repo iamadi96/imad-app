9eea23f5eff976d6640bcc7ba736694c9c627edb
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-26 16:04:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,13 +27,31 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-   var names = ['name1', 'name2', 'name3', 'name4'];
-   var list = '';
-   for(var i=0; i<names.length; i++){
-     list += '<li>' + names[i] + '</li>';   
-   }
-   var ul = document.getElementById('namelist');
-   ul.innerHTML = list;
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture a response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200) {
+               var names = request.responseText;
+               names = JSON.parse(names);
+               var list = '';
+               for(var i=0; i<names.length; i++){
+                 list += '<li>' + names[i] + '</li>';   
+               }
+               var ul = document.getElementById('namelist');
+               ul.innerHTML = list;
+            }
+        }
+      //not done yet    
+    };
+    //make the request
+request.open('GET', 'http://ap96adi.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+
+  
 };
     
     //make a request to the server to send names.
