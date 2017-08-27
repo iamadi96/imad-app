622fe0cc5f54c41242b4f37ed877aa4462c97a2c
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-27 14:13:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config ={
+    user: 'ap96adi',
+    database: 'ap96adi',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD 
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -83,6 +92,19 @@ var template = function(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res){
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM test', function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send(JSON.stringify(result));
+       }
+   });
 });
 
 var counter = 0;    
