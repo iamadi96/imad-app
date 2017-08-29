4340ca48062b9720fadd0dcd884f358fbcc7662f
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-29 21:42:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,19 @@ app.get('/hash/:input', function(req, res){
    res.send(hashedString);
 });
 
+app.post('/create-user', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).tostring('hex');
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, %2)', [username, dbString], function (err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send('User successfully created: ' + username);
+       } 
+    });
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
    // make a select request
