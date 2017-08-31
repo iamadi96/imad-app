b72a3daf449813112067c53a4eb6b81b9b1961e6
Aditya Pandey <30765681+iamadi96@users.noreply.github.com>
2017-08-31 17:24:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.post('/login', function(req, res){
            res.status(500).send(err.toString());
        } else {
            if(result.rows.length === 0){
-               res.send(403).send('username/password is invalid');
+               res.status(403).send('username/password is invalid');
            }else {
              var dbString = result.rows[0].password;
              var salt = dbString.split('$')[2];
@@ -139,7 +139,7 @@ app.post('/login', function(req, res){
              if(hashedPasword === dbString){
              res.send('credentials are correct');
                 }else {
-                    res.send(403).send('username/password is invalid');
+                    res.status(403).send('username/password is invalid');
                 }
             }
        } 
